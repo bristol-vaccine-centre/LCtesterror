8a9c8224abb8d5ae7950de6fb7d98f037ea827ad
Katie Lihou <kl14574@bristol.ac.uk>
2025-04-23 12:26:49 +0100

    added test with delay

--- tests/testthat/test-test_function.R
+++ tests/testthat/test-test_function.R
@@ -12,7 +12,6 @@ test_that("LC model runs with sim data (no delay or dependence)", {
   data_ID = "sim"
 
   sim_results <- sim.test.data(disease_prev = 0.2, sim_size = 1000, test_params = test_params)
-  sim_results
 
   test <- run.LC.model(data=sim_results$test_results, num_tests=num_tests,
                        iter=iter, chains=chains, warmup=warmup)
@@ -21,3 +20,24 @@ test_that("LC model runs with sim data (no delay or dependence)", {
 
 })
 
+test_that("LC model runs with sim data with delay (no dependence)", {
+
+  test_params <- list(test1 = list(sens = 0.95, spec = 0.98, p_performed = 1), test2 = list(sens = 0.90, spec = 0.97, p_performed = 0.8),
+                      test3 = list(sens = 0.95, spec = 0.98, p_performed = 1), test4 = list(sens = 0.90, spec = 0.97, p_performed = 0.8))
+
+  test_params
+
+  num_tests=4
+  iter=500
+  chains=2
+  warmup=200
+  data_ID = "sim"
+
+  sim_results <- sim.test.data(disease_prev = 0.2, sim_size = 1000, test_params = test_params, delay = TRUE)
+
+  test <- run.LC.model(data=sim_results$test_results, num_tests=num_tests,
+                       iter=iter, chains=chains, warmup=warmup, covariates = c("delay"))
+
+  expect_false(is.null(test$stan_fit))
+
+})
