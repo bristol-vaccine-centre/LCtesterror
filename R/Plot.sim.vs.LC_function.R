df87da82deb020843f7f475c90a0cc5f3d6a82d0
Katie Lihou <kl14574@bristol.ac.uk>
2025-05-07 16:55:16 +0100

    tweaked plots

--- R/Plot.sim.vs.LC_function.R
+++ R/Plot.sim.vs.LC_function.R
@@ -5,7 +5,7 @@
 #' Will plot across any simulated any value of the probability of the test occurring (sim_prob),
 #' so if multiple p_performed were simulated in run.sims.LC(), these will need to be pre-filtered to plot each sim_prob paramter value one at a time.
 #'
-#' @param sim_stan_results Dataframe output from run.sims.LC()$stan_results_df.
+#' @param sim_stan_results Named output from run.sims.LC().
 #'
 #' @return Plots of inferred parameters against simulated parameters. Returns 3 plots:
 #' #' \describe{
@@ -13,8 +13,8 @@
 #'   \item{2) Sensitivity}{LC model inferred test sensitivities (mean across chains, median across iterations) vs simulated test sensitivities;}
 #'   \item{3) Specificity}{LC model inferred test specificities (mean across chains, median across iterations) vs simulated test specificities;}
 #' }
-#' @importFrom ggplot2 ggplot theme theme_classic geom_abline geom_point geom_errorbar facet_grid labs coord_cartesian element_text element_rect element_blank unit label_both
-#' @importFrom dplyr rename
+#' @importFrom ggplot2 ggplot theme theme_classic theme_linedraw geom_abline geom_point geom_errorbar facet_grid labs coord_cartesian element_text element_rect element_blank unit label_both scale_color_manual scale_color_brewer
+#' @importFrom dplyr rename left_join filter
 #' @importFrom magrittr %>%
 #' @importFrom tibble tibble
 #' @importFrom utils globalVariables
@@ -24,14 +24,14 @@
 #' if (interactive()) {
 #' sim_results <- run.sims.LC(num_tests = 4,
 #'                        prev_vec = c(0.1, 0.2),
-#'                        spec_vec = c(1),
+#'                        spec_vec = c(1, 0.98),
 #'                        sens_vec = c(1),
 #'                        p_performed_vec= c(1),
 #'                        sim_size = 100,
 #'                        chains = 2,
 #'                        data_ID = "sims")
 #'
-#' sim.result.plots(sim_results$stan_results_df)
+#' sim.result.plots(sim_results)
 #' }
 #'
 
@@ -43,15 +43,20 @@ sim.result.plot <- function(sim_stan_results) {
 
   plots <- list()
 
-  sim_stan_results <- sim_stan_results %>%
+
+  sim_stan_results_combined <- dplyr::left_join(sim_stan_results$stan_results_df, sim_stan_results$sim_inputs, by = c("model_id", "test_id"))
+
+
+  sim_stan_results_combined <-  sim_stan_results_combined %>%
     dplyr::rename(Simulated_prev = sim_prev,
                   Simulated_sens = sim_sens,
                   Simulated_spec = sim_spec,
-                  Test_ID = test_id)
+                  Test_ID = test_id,
+                  Simulated_overall_test_positivity = overall_test_positivity_sim)
 
-  theme <- theme_classic() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
+  theme <- theme_linedraw() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
                               strip.background = element_rect(fill = "white"),
-                              strip.text = element_text(size = 11),
+                              strip.text = element_text(size = 11, colour = "black"),
                               panel.grid.major = element_blank(),
                               panel.grid.minor = element_blank(),
                               legend.key = element_rect(fill = "white", colour = NA),
@@ -61,39 +66,47 @@ sim.result.plot <- function(sim_stan_results) {
                               axis.title = element_text(size = 14))
 
 
-  plot_prev <- sim_stan_results %>%
-    ggplot(aes(x = Simulated_prev, y = stan_prev)) +
+  plot_prev <- sim_stan_results_combined %>%
+    dplyr::filter(Test_ID == "test1") %>% #all tests have the same results
+    ggplot(aes(x = Simulated_prev)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
-    geom_point(shape=21, fill="black", colour="black", size=1) +
-    geom_errorbar(aes(ymin = stan_prev_CI_low, ymax = stan_prev_CI_high), width = 0, colour="black", linewidth=1, alpha=0.6) +
+    geom_point(aes(y = stan_prev, colour="Inferred prevalence"), shape=21, fill="black", size=1) +
+    geom_errorbar(aes(ymin = stan_prev_CI_low, ymax = stan_prev_CI_high, colour="Inferred prevalence"), width = 0, linewidth=1, alpha=0.8) +
+    #overall test positivity (assuming any test positive is a positive):
+    geom_point(aes(y = Simulated_overall_test_positivity, color = "Test positivity"), shape=21, fill="tomato2", size=0.8, alpha=0.8) +
+    geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper, color = "Test positivity"), width = 0, linewidth=0.8, alpha=0.6) +
     facet_grid(Simulated_spec ~ Simulated_sens, scales = "free",
                labeller = label_both) +
+  scale_color_manual(name = "Estimate type",
+      values = c("Inferred prevalence" = "black", "Test positivity" = "tomato2")) +
     labs(title = "Simuated vs inferred prevalence", x = "Simulated prevalence", y = "Inferred prevalence") +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
 
   plots$prev <- plot_prev
 
-  plot_sens_median <- sim_stan_results %>%
+  plot_sens_median <- sim_stan_results_combined %>%
     ggplot(aes(x = Simulated_sens, y = sens_median, colour=Test_ID)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
     geom_point(size=1, alpha=0.8) +
-    geom_errorbar(aes(ymin = sens_median_CI_low, ymax = sens_median_CI_high), width = 0, linewidth=1,alpha=0.5) +
+    geom_errorbar(aes(ymin = sens_median_CI_low, ymax = sens_median_CI_high), width = 0, linewidth=1,alpha=0.4) +
     facet_grid(Simulated_spec ~ Simulated_prev, scales = "free",
                labeller = label_both) +
+    scale_color_brewer(palette = "Dark2") +
     labs(title = "Simuated vs inferred sensitivity", x = "Simulated sensitivity", y = "Inferred sensitivity") +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
 
   plots$sens_median <-  plot_sens_median
 
-  plot_spec_median <-  sim_stan_results %>%
+  plot_spec_median <-  sim_stan_results_combined %>%
     ggplot(aes(x = Simulated_spec, y = spec_median, colour=Test_ID)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
     geom_point(size=1, alpha=0.8) +
-    geom_errorbar(aes(ymin = spec_median_CI_low, ymax = spec_median_CI_high), width = 0, linewidth=1, alpha=0.5) +
+    geom_errorbar(aes(ymin = spec_median_CI_low, ymax = spec_median_CI_high), width = 0, linewidth=1, alpha=0.4) +
     facet_grid(Simulated_sens ~ Simulated_prev, scales = "free",
                labeller = label_both) +
+    scale_color_brewer(palette = "Dark2") +
     labs(title = "Simuated vs inferred specificity", x = "Simulated specificity", y = "Inferred specificity") +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
