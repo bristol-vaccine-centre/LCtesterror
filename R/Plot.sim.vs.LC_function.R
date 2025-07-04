e3cfbd024e59194bba978760550bee23ed24c81b
Katie Lihou <kl14574@bristol.ac.uk>
2025-07-04 11:54:50 +0100

    formatted sim plots

--- R/Plot.sim.vs.LC_function.R
+++ R/Plot.sim.vs.LC_function.R
@@ -16,7 +16,7 @@
 #'   \item{3) Specificity}{LC model inferred test specificities (mean across chains, median across iterations) vs simulated test specificities;}
 #' }
 #' @importFrom ggplot2 ggplot theme theme_classic theme_linedraw geom_abline geom_point geom_errorbar facet_grid labs coord_cartesian element_text element_rect element_blank unit label_both scale_color_manual scale_color_brewer
-#' @importFrom dplyr rename left_join filter
+#' @importFrom dplyr rename left_join filter case_when
 #' @importFrom magrittr %>%
 #' @importFrom tibble tibble
 #' @importFrom utils globalVariables
@@ -45,18 +45,28 @@ sim.result.plot <- function(sim_stan_results) {
 
   plots <- list()
 
-
   sim_stan_results_combined1 <- dplyr::left_join(sim_stan_results$stan_results_df, sim_stan_results$sim_inputs, by = c("model_id", "test_id"))
   sim_stan_results_combined2 <- left_join(sim_stan_results_combined1 , sim_stan_results$divergence_summary, by = c("model_id"))
 
-
   sim_stan_results_combined <-  sim_stan_results_combined2 %>%
-    dplyr::rename(Simulated_prev = sim_prev,
-                  Simulated_sens = sim_sens,
-                  Simulated_spec = sim_spec,
+    dplyr::rename(Prev = sim_prev,
+                  Sens = sim_sens,
+                  Spec = sim_spec,
                   Test_ID = test_id,
                   Simulated_overall_test_positivity = overall_test_positivity_sim) %>%
-    mutate(Inferred_prev_divergent = factor(ifelse((divergent_count > 0 | low_ess == TRUE), "Not converged", "Inferred prevalence")))
+    mutate(Inferred_prev_divergent = factor(ifelse((divergent_count > 0 | low_ess == TRUE), "Not converged", "Inferred prevalence"))) %>%
+    mutate(Test_ID_divergent = factor(ifelse((divergent_count > 0 | low_ess == TRUE), "Not converged", Test_ID)))
+
+  #colours
+  dark2_cols <- RColorBrewer::brewer.pal(n = 8, name = "Dark2")
+  # list of test_id levels
+  test_ids <- sim_stan_results_combined %>%
+    filter(Test_ID_divergent != "Not converged") %>%
+    pull(Test_ID_divergent) %>%
+    unique()
+  # named color vector
+  named_colors <- setNames(dark2_cols[1:length(test_ids)], test_ids)
+  named_colors["Not converged"] <- "grey85"
 
   theme <- theme_linedraw() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
                               strip.background = element_rect(fill = "white"),
@@ -72,49 +82,47 @@ sim.result.plot <- function(sim_stan_results) {
 
   plot_prev <- sim_stan_results_combined %>%
     dplyr::filter(Test_ID == "test1") %>% #all tests have the same results
-    ggplot(aes(x = Simulated_prev)) +
-    geom_abline(intercept = 0, slope = 1, colour = "grey") +
+    ggplot(aes(x = Prev)) +
+    geom_abline(intercept = 0, slope = 1, colour = "#D55E00", alpha=0.5) +
     geom_point(aes(y = stan_prev, colour=Inferred_prev_divergent, fill=Inferred_prev_divergent), shape=21, size=1) +
     geom_errorbar(aes(ymin = stan_prev_CI_low, ymax = stan_prev_CI_high, colour=Inferred_prev_divergent), width = 0, linewidth=0.5, alpha=0.5) +
     #overall test positivity (assuming any test positive is a positive):
-    geom_point(aes(y = Simulated_overall_test_positivity, color = "Test positivity"), shape=21, fill="tomato2", size=0.8, alpha=0.8) +
+    geom_point(aes(y = Simulated_overall_test_positivity, color = "Test positivity", fill="Test positivity"), shape=21, size=0.8, alpha=0.8) +
     geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper, color = "Test positivity"), width = 0, linewidth=0.5, alpha=0.5) +
-    facet_grid(Simulated_spec ~ Simulated_sens, scales = "free",
+    facet_grid(Spec ~ Sens, scales = "free",
                labeller = label_both) +
-    scale_color_manual(name = "Estimate type",
-                       values = c("Inferred prevalence" = "black", "Not converged" = "grey85", "Test positivity" = "tomato2")) +
-    scale_fill_manual(name = "Estimate type",
-                      values = c("Inferred prevalence" = "black", "Not converged" = "grey85", "Test positivity" = "tomato2")) +
-    labs(title = "Simuated vs inferred prevalence", x = "Simulated prevalence", y = "Inferred prevalence") +
+    scale_color_manual(values = c("Inferred prevalence" = "black", "Not converged" = "grey85", "Test positivity" = "#009E73")) +
+    scale_fill_manual(values = c("Inferred prevalence" = "black", "Not converged" = "grey85", "Test positivity" = "#009E73")) +
+    labs(title = "Simuated vs inferred prevalence", x = "Simulated prevalence", y = "Inferred prevalence", color = NULL, fill = NULL) +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme +
-    guides(fill = "none", color = guide_legend(title = "Estimate type") )
+    guides(fill = "none", color = guide_legend() )
 
   plots$prev <- plot_prev
 
   plot_sens_median <- sim_stan_results_combined %>%
-    ggplot(aes(x = Simulated_sens, y = sens_median, colour=Test_ID)) +
+    ggplot(aes(x = Sens, y = sens_median, colour=Test_ID_divergent)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
     geom_point(size=1, alpha=0.8) +
     geom_errorbar(aes(ymin = sens_median_CI_low, ymax = sens_median_CI_high), width = 0, linewidth=0.5,alpha=0.4) +
-    facet_grid(Simulated_spec ~ Simulated_prev, scales = "free",
+    facet_grid(Spec ~ Prev, scales = "free",
                labeller = label_both) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(title = "Simuated vs inferred sensitivity", x = "Simulated sensitivity", y = "Inferred sensitivity") +
+    scale_color_manual(values = named_colors) +
+    labs(title = "Simulated vs inferred sensitivity", x = "Simulated sensitivity", y = "Inferred sensitivity", color = NULL, fill = NULL) +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
 
   plots$sens_median <-  plot_sens_median
 
   plot_spec_median <-  sim_stan_results_combined %>%
-    ggplot(aes(x = Simulated_spec, y = spec_median, colour=Test_ID)) +
+    ggplot(aes(x = Spec, y = spec_median, colour=Test_ID_divergent)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
     geom_point(size=1, alpha=0.8) +
     geom_errorbar(aes(ymin = spec_median_CI_low, ymax = spec_median_CI_high), width = 0, linewidth=0.5, alpha=0.4) +
-    facet_grid(Simulated_sens ~ Simulated_prev, scales = "free",
+    facet_grid(Sens ~ Prev, scales = "free",
                labeller = label_both) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(title = "Simuated vs inferred specificity", x = "Simulated specificity", y = "Inferred specificity") +
+    scale_color_manual(values = named_colors) +
+    labs(title = "Simulated vs inferred specificity", x = "Simulated specificity", y = "Inferred specificity", color = NULL, fill = NULL) +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
 
@@ -142,7 +150,7 @@ sim.result.plot <- function(sim_stan_results) {
 #'   \item{3) Specificity}{LC model inferred test specificities (mean across chains, median across iterations) vs simulated test specificities;}
 #' }
 #' @importFrom ggplot2 ggplot theme theme_classic theme_linedraw geom_abline geom_point geom_errorbar facet_grid labs coord_cartesian element_text element_rect element_blank unit label_both scale_color_manual scale_color_brewer
-#' @importFrom dplyr rename left_join filter
+#' @importFrom dplyr rename left_join filter case_when
 #' @importFrom magrittr %>%
 #' @importFrom tibble tibble
 #' @importFrom utils globalVariables
@@ -173,15 +181,27 @@ sim.result.time.plot <- function(sim_stan_results) {
 
   plots <- list()
 
+  sim_stan_results_combined1 <- dplyr::left_join(sim_stan_results$stan_results_df, sim_stan_results$sim_inputs, by = c("model_id", "test_id"))
+  sim_stan_results_combined2 <- left_join(sim_stan_results_combined1 , sim_stan_results$divergence_summary, by = c("model_id"))
 
-  sim_stan_results_combined <- dplyr::left_join(sim_stan_results$stan_results_df, sim_stan_results$sim_inputs, by = c("model_id", "test_id"))
-
-
-  sim_stan_results_combined <-  sim_stan_results_combined %>%
-    dplyr::rename(Simulated_sens = sim_sens,
-                  Simulated_spec = sim_spec,
+  sim_stan_results_combined <-  sim_stan_results_combined2 %>%
+    dplyr::rename(Sens = sim_sens,
+                  Spec = sim_spec,
                   Test_ID = test_id,
-                  Simulated_overall_test_positivity = overall_test_positivity_sim)
+                  Simulated_overall_test_positivity = overall_test_positivity_sim) %>%
+    mutate(Inferred_prev_divergent = factor(ifelse((divergent_count > 0 | low_ess == TRUE), "Not converged", "Inferred prevalence"))) %>%
+    mutate(Test_ID_divergent = factor(ifelse((divergent_count > 0 | low_ess == TRUE), "Not converged", Test_ID)))
+
+  #colours
+  dark2_cols <- RColorBrewer::brewer.pal(n = 8, name = "Dark2")
+  # list of test_id levels
+  test_ids <- sim_stan_results_combined %>%
+    filter(Test_ID_divergent != "Not converged") %>%
+    pull(Test_ID_divergent) %>%
+    unique()
+  # named color vector
+  named_colors <- setNames(dark2_cols[1:length(test_ids)], test_ids)
+  named_colors["Not converged"] <- "grey85"
 
   theme <- theme_linedraw() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
                                     strip.background = element_rect(fill = "white"),
@@ -195,34 +215,34 @@ sim.result.time.plot <- function(sim_stan_results) {
                                     axis.title = element_text(size = 14))
 
 
-
   plot_sens_median <- sim_stan_results_combined %>%
-    ggplot(aes(x = Simulated_sens, y = sens_median, colour=Test_ID)) +
+    ggplot(aes(x = Sens, y = sens_median, colour=Test_ID_divergent)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
     geom_point(size=1, alpha=0.8) +
     geom_errorbar(aes(ymin = sens_median_CI_low, ymax = sens_median_CI_high), width = 0, linewidth=0.5,alpha=0.4) +
-    facet_grid(~Simulated_spec, scales = "free",
+    facet_grid(~Spec, scales = "free",
                labeller = label_both) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(title = "Simuated vs inferred sensitivity", x = "Simulated sensitivity", y = "Inferred sensitivity") +
+    scale_color_manual(values = named_colors) +
+    labs(title = "Simulated vs inferred sensitivity", x = "Simulated sensitivity", y = "Inferred sensitivity", color = NULL, fill = NULL) +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
 
   plots$sens_median <-  plot_sens_median
 
   plot_spec_median <-  sim_stan_results_combined %>%
-    ggplot(aes(x = Simulated_spec, y = spec_median, colour=Test_ID)) +
+    ggplot(aes(x = Spec, y = spec_median, colour=Test_ID_divergent)) +
     geom_abline(intercept = 0, slope = 1, colour = "grey") +
     geom_point(size=1, alpha=0.8) +
     geom_errorbar(aes(ymin = spec_median_CI_low, ymax = spec_median_CI_high), width = 0, linewidth=0.5, alpha=0.4) +
-    facet_grid(~Simulated_sens, scales = "free",
+    facet_grid(~Sens, scales = "free",
                labeller = label_both) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(title = "Simuated vs inferred specificity", x = "Simulated specificity", y = "Inferred specificity") +
+    scale_color_manual(values = named_colors) +
+    labs(title = "Simulated vs inferred specificity", x = "Simulated specificity", y = "Inferred specificity", color = NULL, fill = NULL) +
     coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     theme
 
   plots$spec_median <- plot_spec_median
+
 
 #prev plot over time:
   sim_data <- sim_stan_results$sim_data %>%
@@ -246,27 +266,32 @@ sim.result.time.plot <- function(sim_stan_results) {
     )
   stan_data <- sim_stan_results$stan_results_df %>%
     filter(test_id == "test1")
-  stan_sim_df_weekly <- dplyr::left_join(stan_data, sim_data_week, by=c('week', 'model_id'))
+
+  stan_sim_df_weekly1 <- dplyr::left_join(stan_data, sim_data_week, by=c('week', 'model_id'))
+  stan_sim_df_weekly2 <- dplyr::left_join(stan_sim_df_weekly1, sim_stan_results$divergence_summary, by = c("model_id"))
+
+  stan_sim_df_weekly <- stan_sim_df_weekly2 %>%
+    mutate(Inferred_prev_divergent = factor(ifelse((divergent_count > 0 | low_ess == TRUE), "Not converged", "Inferred prevalence")))
 
   #what if sim_prob is different for the same sens/spec?
   weekly_prev_plot <- stan_sim_df_weekly %>%
     dplyr::rename(Spec = "sim_spec", Sens = "sim_sens") %>%
     ggplot(aes(x = week)) +
-    geom_point(aes(y = stan_prev, color = "Inferred prevalence")) +
-    geom_ribbon(aes(ymin = stan_prev_CI_low, ymax = stan_prev_CI_high), alpha = 0.2) +  #inferred prev CI's
-    geom_line(aes(y = weekly_true_prev, color = "True prevalence"), linewidth = 1, alpha=0.8) +
-    geom_point(aes(y = overall_weekly_test_positivity, color = "Apparent prevalence"), alpha=0.8) +
-    geom_smooth(aes(y = overall_weekly_test_positivity, color = "Apparent prevalence"), se = FALSE, method = "loess", alpha=0.8) +
+    geom_point(aes(y = stan_prev, colour=Inferred_prev_divergent), size=1) +
+    geom_ribbon(aes(ymin = stan_prev_CI_low, ymax = stan_prev_CI_high, fill = Inferred_prev_divergent), alpha = 0.4, colour=NA) +  #inferred prev CI's
+    geom_line(aes(y = weekly_true_prev, color = "True prevalence"),  alpha=0.6, linewidth = 1) +
+    geom_point(aes(y = overall_weekly_test_positivity, color = "Test positivity"), size=1, alpha=0.6) +
+    geom_line(aes(y = overall_weekly_test_positivity, color = "Test positivity"), stat="smooth", method="loess", alpha=0.6, linewidth = 1) + #using geom_line instead of geom_smooth so can use alpha on line colour
     facet_grid(Spec ~ Sens, scales = "free",
                labeller = label_both) +
-    scale_color_manual(values = c(
-                          "Inferred prevalence" = "black",
-                          "True prevalence" = "red",
-                          "Apparent prevalence" = "blue")) +
-    labs(y = "Prevalence", x = "Week") +
-    theme
+    scale_color_manual(values = c("Inferred prevalence" = "black", "Not converged" = "grey", "True prevalence" = "#D55E00", "Test positivity" = "#009E73")) +
+    scale_fill_manual(values = c("Inferred prevalence" = "black", "Not converged" = "grey", "Test positivity" = "#009E73")) +
+    labs(y = "Prevalence", x = "Week", color = NULL, fill = NULL) +
+    theme +
+    guides(fill = "none", color = guide_legend() )
 
 plots$prev_plot <- weekly_prev_plot
+
   return(plots)
 }
 
